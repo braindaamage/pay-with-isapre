981a17deeb11d71e7df6f0d873fc2348a6d95b5f
Leonardo Olivares <leonardo@acid.cl>
2017-03-22 15:36:38 -0300

    add initialize method for consoled payment class

--- lib/isapres/consalud/payment.rb
+++ lib/isapres/consalud/payment.rb
@@ -1,7 +1,15 @@
 module PWI
   module Consalud
     class Payment
-      
+      # Public: Loads the configuration file consalud.yml
+      # If it's a rails application it will take the file from the config/ directory
+      #
+      # env - Environment.
+      #
+      # Returns a Config object.
+      def initialize env = nil
+        @@config ||= PWI::Consalud::Config.new(env)
+      end
     end
   end
 end
