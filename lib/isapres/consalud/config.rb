2c7c5c045a821d3b5c002f32030ab2ba6a8d15ed
Leonardo Olivares <leonardo@acid.cl>
2017-03-22 16:32:50 -0300

    Implement PWI::Config class to config class in consalud

--- lib/isapres/consalud/config.rb
+++ lib/isapres/consalud/config.rb
@@ -2,24 +2,13 @@ require 'yaml'
 
 module PWI
   module Consalud
-    class Config
-      attr_accessor :config_filepath, :ws_check_url, :payment_url, :ws_reduce_url
-
-      def initialize env = nil
-        if env
-          # For non-rails apps
-          @config_filepath = File.join(File.dirname(__FILE__), "..", "..", "..", "config", "consalud.yml")
-          load(env)
-        else
-          @config_filepath = File.join(Rails.root, "config", "consalud.yml")
-          load(Rails.env)
-        end
-      end
+    class Config < PWI::Config
+      attr_accessor :ws_check_url, :payment_url, :ws_reduce_url
 
       private
-
+      
         def load(rails_env)
-          config = YAML.load_file(@config_filepath)[rails_env]
+          config = YAML.load_file(@config_filepath)['consalud'][rails_env]
           @ws_check_url   = config['ws_check']
           @payment_url    = config['payment']
           @ws_reduce_url  = config['ws_reduce']
